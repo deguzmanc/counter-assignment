ea8cd3a8182cecf40a3235207dc7bded2c838d59
deguzmanc <deguzmanc@ufl.edu>
2023-04-08 15:32:33 -0400

    Updated Counter tests to have full coverage

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,44 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Counter from "../components/Counter";
+
+let counter;
+let increaseButton;
+let decreaseButton;
 
 beforeEach(() => {
   // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  counter = screen.getByTestId(/count/);
+  increaseButton = screen.getByText("+");
+  decreaseButton = screen.getByText("-");
 });
 
-test('should render initial count with value of 0', () => {
+test("renders counter message", () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByRole("heading")).toHaveTextContent("Counter");
 });
 
-test('clicking + increments the count', () => {
+test("should render initial count with value of 0", () => {
   // Complete the unit test below based on the objective in the line above
+  expect(counter).toHaveTextContent(/0/);
 });
 
-test('clicking - decrements the count', () => {
+test("clicking + increments the count", async () => {
   // Complete the unit test below based on the objective in the line above
+  const count = parseInt(counter);
+  await userEvent.click(increaseButton);
+
+  //Assert
+  expect(parseInt(counter)).toBe(count + 1);
+});
+
+test("clicking - decrements the count", async () => {
+  const count = parseInt(counter);
+  await userEvent.click(decreaseButton);
+
+  //Assert
+  expect(parseInt(counter)).toBe(count - 1);
 });
